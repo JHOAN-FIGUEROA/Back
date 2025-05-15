bcba0360d6db7ac6c1590cfc2db865510db6f2ee
lafigura <jhoan24figueroa@gmail.com>
2025-05-15 12:30:02 -0500

    config

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const sequelize = require('./config/database');
 require('dotenv').config();
+const cors = require('cors');
+app.use(cors());
+
 
 app.use(express.json());
 
